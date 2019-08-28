b01983c410ac8ecb7aae8326782bb88dff6c0b0a
Kyle Kirst <kyle.kirst@gmail.com>
2019-08-28 12:35:48 -0700

    Util module with tests

--- src/js/util.js
+++ src/js/util.js
@@ -1,5 +1,36 @@
+import isNil from 'lodash.isnil';
+import isPlainObject from 'lodash.isplainobject';
+
 module.exports = {
-	isString: function(str) {
+	isElement: o => {
+		return typeof HTMLElement === 'object'
+			? o instanceof HTMLElement //DOM2
+			: o &&
+					typeof o === 'object' &&
+					o !== null &&
+					o.nodeType === 1 &&
+					typeof o.nodeName === 'string';
+	},
+	isEmpty: item => {
+		if (isNil(item)) {
+			return true;
+		}
+
+		if (item === '') {
+			return true;
+		}
+
+		if (Array.isArray(item) && item.length === 0) {
+			return true;
+		}
+
+		if (isPlainObject(item) && Object.keys(item).length === 0) {
+			return true;
+		}
+
+		return false;
+	},
+	isString: str => {
 		return typeof str === 'string';
 	},
 };
--- tests/util.spec.js
+++ tests/util.spec.js
@@ -1,11 +1,66 @@
 import util from '../src/js/util';
 
 describe('util', () => {
-	test('isString returns true when passed a string', () => {
-		expect(util.isString('')).toBe(true);
+	describe('isElement', () => {
+		test('returns false when passing a string', () => {
+			expect(util.isElement('<div></div>')).toBe(false);
+		});
+
+		test('returns false when passing an object', () => {
+			expect(util.isElement({ style: 'fake style property' })).toBe(false);
+		});
+
+		test('returns true when passing a real HTML Element', () => {
+			const testElement = document.createElement('div');
+			expect(util.isElement(testElement)).toBe(true);
+		});
 	});
 
-	test('isString returns false when passed a number', () => {
-		expect(util.isString(10)).toBe(false);
+	describe('isEmpty', () => {
+		test('return true if null', () => {
+			expect(util.isEmpty(null)).toBe(true);
+		});
+
+		test('return true if undefined', () => {
+			expect(util.isEmpty(undefined)).toBe(true);
+		});
+
+		test('return true if empty string', () => {
+			expect(util.isEmpty('')).toBe(true);
+		});
+
+		test('return true if empty array', () => {
+			expect(util.isEmpty([])).toBe(true);
+		});
+
+		test('return true if empty object', () => {
+			expect(util.isEmpty({})).toBe(true);
+		});
+
+		test('return false if string with characters', () => {
+			expect(util.isEmpty('Not Empty!')).toBe(false);
+		});
+
+		test('return false if object with at least 1 property', () => {
+			expect(util.isEmpty({ thing: 'thing 2' })).toBe(false);
+		});
+	});
+
+	describe('isString', () => {
+		test('returns false when passed null', () => {
+			expect(util.isString(null)).toBe(false);
+		});
+
+		test('returns false when passed int', () => {
+			expect(util.isString(1)).toBe(false);
+		});
+
+		test('returns false when passed object', () => {
+			expect(util.isString({})).toBe(false);
+		});
+
+		test('returns true when passed string', () => {
+			expect(util.isString('Hi!')).toBe(true);
+		});
 	});
 });
