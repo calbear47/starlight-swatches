56dc1cba17430849e5a6ba400bfbb0b2daf2d65c
Kyle Kirst <kyle.kirst@gmail.com>
2019-08-28 12:34:25 -0700

    Swatcher main instance init flow with tests

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,70 @@
 import util from './util';
 import '../scss/app.scss'; // Ensures that our scss is built and bundled on compilation.
 
-function MyProject(str) {
-	this.thing = str || 'happy-too';
+function BuildSwatcher(options) {
+	const required_options = ['thirdparty_api', 'selector_id'];
+
+	this._options = {
+		container: null,
+		thirdparty_api: '',
+		selector_id: '',
+		image_id: '',
+		swatch_width: 30,
+	};
+
+	if (options.container === null) {
+		throw new Error('You must initialize BuildSwatcher with a container.');
+	}
+
+	if (util.isElement(options.container)) {
+		// go find container and check validity
+		const isValid = this._verifyOptions(required_options, options);
+		if (isValid) {
+			// setup swatcher
+		}
+	}
+
+	if (util.isString(options.container)) {
+		const foundElement = document.getElementById(options.container);
+		if (util.isElement(foundElement)) {
+			const isValid = this._verifyOptions(required_options, options);
+			if (isValid) {
+				// setup swatcher
+			}
+		} else {
+			throw new Error(
+				`Build Swatcher can't find the id "${options.container}". Make sure the DOM has been loaded prior to initialization.`,
+			);
+		}
+	}
+
+	if (!util.isString(options.container) && !util.isElement(options.container)) {
+		throw new Error(
+			'Build Swatcher was initialized incorrectly. Pass an HTML Element or element id<string>.',
+		);
+	}
 }
 
-MyProject.prototype.fooMethod = function() {
-	return util.isString(this.thing);
+/** Helper Methods **/
+
+BuildSwatcher.prototype._setupSwatcher = function(options) {
+	this._options = Object.assign({}, this._options, options);
 };
 
-module.exports = MyProject;
+BuildSwatcher.prototype._verifyOptions = function(required, options) {
+	for (let i = 0; i < required.length; i++) {
+		if (
+			Object.prototype.hasOwnProperty.call(options, required[i]) &&
+			!util.isEmpty(options[required[i]])
+		) {
+			continue;
+		} else {
+			throw new Error(
+				`Build Swatcher was initialized without a required option: ${required[i]}`,
+			);
+		}
+	}
+	return true;
+};
+
+module.exports = BuildSwatcher;
--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,15 +1,116 @@
-import MyProject from '../src/js/index';
-import util from '../src/js/util';
+import BuildSwatcher from '../src/js/index';
 
-describe('MyProject', () => {
-	test('fooMethod checks thing', () => {
-		jest.spyOn(util, 'isString');
-		util.isString.mockImplementation = jest.fn(() => true);
+describe('Build Swatcher', () => {
+	describe('Instance', () => {
+		test('throws error if container is null', () => {
+			expect(() => {
+				return new BuildSwatcher({
+					container: null,
+					thirdparty_api: 'https://apime.com/',
+				});
+			}).toThrowError(
+				new Error('You must initialize BuildSwatcher with a container.'),
+			);
+		});
 
-		const project = new MyProject('haha');
-		expect(project.fooMethod()).toBe(true);
+		test('throws error if container property is not passed', () => {
+			expect(() => {
+				return new BuildSwatcher({
+					thirdparty_api: 'https://apime.com/',
+					selector_id: '',
+				});
+			}).toThrowError(
+				new Error(
+					'Build Swatcher was initialized incorrectly. Pass an HTML Element or element id<string>.',
+				),
+			);
+		});
 
-		expect(util.isString).toHaveBeenCalledTimes(1);
-		expect(util.isString).toHaveBeenCalledWith('haha');
+		test('throws error if container string id cannot be found in the DOM', () => {
+			expect(() => {
+				return new BuildSwatcher({
+					container: 'swatcher-container-id',
+					thirdparty_api: 'https://apime.com/',
+					selector_id: '',
+				});
+			}).toThrowError(
+				new Error(
+					`Build Swatcher can't find the id "swatcher-container-id". Make sure the DOM has been loaded prior to initialization.`,
+				),
+			);
+		});
+
+		test('throws error if a required property is empty', () => {
+			const containerElement = document.createElement('div');
+			containerElement.id = 'swatcher-container-id';
+			document.body.appendChild(containerElement);
+			expect(() => {
+				return new BuildSwatcher({
+					container: 'swatcher-container-id',
+					thirdparty_api: 'https://apime.com/',
+					selector_id: '',
+				});
+			}).toThrowError(
+				new Error(
+					`Build Swatcher was initialized without a required option: selector_id`,
+				),
+			);
+		});
+
+		test('creates instance when intialized properly.', () => {
+			const containerElement = document.createElement('div');
+			containerElement.id = 'swatcher-container-id';
+			document.body.appendChild(containerElement);
+
+			const swatcherInstance = new BuildSwatcher({
+				container: 'swatcher-container-id',
+				thirdparty_api: 'https://apime.com/',
+				selector_id: 'selector-custom-id',
+			});
+
+			expect(swatcherInstance).toBeInstanceOf(BuildSwatcher);
+		});
+	});
+
+	describe('_verifyOptions', () => {
+		test('returns true when all required options are not empty in options object', () => {
+			const containerElement = document.createElement('div');
+			containerElement.id = 'swatcher-container-id';
+			document.body.appendChild(containerElement);
+			const swatcherInstance = new BuildSwatcher({
+				container: 'swatcher-container-id',
+				thirdparty_api: 'https://apime.com/',
+				selector_id: 'selector-custom-id',
+			});
+			expect(
+				swatcherInstance._verifyOptions(['thirdparty_api', 'selector_id'], {
+					container: 'swatcher-container-id',
+					thirdparty_api: 'https://apime.com/',
+					selector_id: 'selector-custom-id',
+				}),
+			).toBe(true);
+		});
+
+		test('throws error if any required options were empty', () => {
+			const containerElement = document.createElement('div');
+			containerElement.id = 'swatcher-container-id';
+			document.body.appendChild(containerElement);
+			const swatcherInstance = new BuildSwatcher({
+				container: 'swatcher-container-id',
+				thirdparty_api: 'https://apime.com/',
+				selector_id: 'selector-custom-id',
+			});
+			expect(() => {
+				swatcherInstance._verifyOptions(['thirdparty_api', 'selector_id'], {
+					container: 'swatcher-container-id',
+					thirdparty_api: '',
+					selector_id: 'selector-custom-id',
+				});
+			}).toThrowError(
+				new Error(
+					`Build Swatcher was initialized without a required option: thirdparty_api`,
+				),
+			);
+		});
 	});
 });
