b1b37735294a1661d36382ae7f8cd31a5a5732ab
Kyle Kirst <kyle.kirst@gmail.com>
2019-08-28 18:21:23 -0700

    updates to babel rc for async/await

--- .babelrc.js
+++ .babelrc.js
@@ -2,7 +2,12 @@ const isProd = String(process.env.NODE_ENV) === 'production';
 const isTest = String(process.env.NODE_ENV) === 'test';
 
 module.exports = {
-	presets: [['@babel/preset-env', { modules: isTest ? 'commonjs' : false }]],
+	presets: [
+		[
+			'@babel/preset-env',
+			{ modules: isTest ? 'commonjs' : false, targets: { node: 'current' } },
+		],
+	],
 	plugins: [
 		'@babel/plugin-syntax-dynamic-import',
 		'@babel/plugin-proposal-class-properties',
